08ce434fd8a88deb362c08712c7edb92dd23991e
Simon Arlott <sa.me.uk>
2022-10-25 21:15:56 +0100

    Don't try to document the logger name

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -26,7 +26,10 @@ namespace uuid {
 
 namespace modbus {
 
+//! @cond false
 static const char __pstr__loggername[] __attribute__((__aligned__(sizeof(int)))) PROGMEM = "modbus";
+//! @endcond
+
 const uuid::log::Logger logger{reinterpret_cast<const __FlashStringHelper *>(__pstr__loggername), uuid::log::Facility::DAEMON};
 
 } // namespace modbus
