1a1fad7d7ee91c57c061ddaa8c0605db6ce35f80
Simon Arlott <sa.me.uk>
2024-03-31 11:30:22 +0100

    add readthedocs config file

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,14 @@
 
 import os
 
+rtd = os.environ.get("READTHEDOCS") == 'True'
+
 needs_sphinx = '1.3'
 extensions = []
 source_suffix = ['.rst']
 
 project = u'mcu-uuid-modbus'
-copyright = u'2021-2022, Simon Arlott'
+copyright = u'2021-2024, Simon Arlott'
 author = u'Simon Arlott'
 
 master_doc = 'index'
@@ -20,6 +22,9 @@ exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 highlight_language = 'c++'
 todo_include_todos = False
+
+if rtd:
+	html_theme = 'sphinx_rtd_theme'
 
 linkcheck_timeout = 60
 linkcheck_ignore = [r'https://github.com/.+/.+/(compare|commits)/.+']
